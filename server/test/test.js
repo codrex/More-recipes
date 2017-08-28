d211465844e0040db0c4ec09e91b3b99d7446853
resy <ogbemudia@gmail.com>
2017-08-28 03:49:55 +0100

    write test for recipe creation

--- server/test/test.js
+++ server/test/test.js
@@ -4,7 +4,7 @@ import supertest from 'supertest';
 import app from '../bin/www.js';
 
 const request = supertest(app);
-
+let token = '';
 // Create account test
 let testData = {};
 
@@ -103,6 +103,7 @@ describe('User Login', () => {
       .send(testDataLogin)
       .end((err, res) => {
         console.log(res.status);
+        token = res.body.token;
         expect(res.status).to.equal(200);
         expect(res.body.status).to.equal('success');
         done();
@@ -137,6 +138,156 @@ describe('User Login', () => {
     request.post('/api/users/signin')
       .send(invalidData)
       .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+});
+
+// add recipe
+describe('add recipe', () => {
+  let recipe = {};
+  beforeEach(() => {
+    recipe = {
+      recipeName: 'beans cake',
+      category: 'breakfast',
+      ingredients: ['beans', 'water', 'oil'],
+      directions: ['step 1', 'step 2', 'step 3'],
+    };
+  });
+  it('return 200 as status code', done => {
+    console.log(recipe);
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(recipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal('success');
+        done();
+      });
+  });
+
+  it('return 400 for  without access token', done => {
+    request.post('/api/recipes/recipe')
+      .send(recipe)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 for invalid access token', done => {
+    const invalidToken = `${token}'dkfjkfjd'`;
+    request.post('/api/recipes/recipe')
+      .set('Authorization', invalidToken)
+      .send(recipe)
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.recipeName = '';
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.category = '';
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.directions = [];
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.ingredients = [];
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.recipeName = 123456;
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.category = undefined;
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.directions = [233, null, 4950];
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(400);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+  });
+  it('return 400 as status code', done => {
+    const invalidRecipe = recipe;
+    invalidRecipe.ingredients = { step: 'one', step2: 'two' };
+    request.post('/api/recipes/recipe')
+      .set('Authorization', token)
+      .send(invalidRecipe)
+      .end((err, res) => {
+        console.log(res.status);
         expect(res.status).to.equal(400);
         expect(res.body.status).to.equal('fail');
         done();
