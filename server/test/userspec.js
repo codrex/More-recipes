a853fb0a3f16170fa345781dcaa3b3429da79949
resy <ogbemudia@gmail.com>
2017-09-15 15:11:51 +0100

    write test case for profile update

--- server/test/userspec.js
+++ server/test/userspec.js
@@ -4,21 +4,21 @@ import { expect } from 'chai';
 const request = supertest(app);
 
 export const userSpec = (repiceSpec) => {
-  let testData = {};
-  let regData = {};
+  let userTwo = {};
+  let userOne = {};
   let token;
   let token2;
 
   // user sign up
   describe('User registration', () => {
     beforeEach(() => {
-      regData = {
+      userOne = {
         fullname: 'example user two',
         username: 'example_user_2',
         password: '123456',
         email: 'exampleTwo@user.com',
       };
-      testData = {
+      userTwo = {
         fullname: 'example user',
         username: 'example_user',
         password: '123456',
@@ -27,7 +27,7 @@ export const userSpec = (repiceSpec) => {
     });
     it('return 200 as status code', done => {
       request.post('/api/v1/users/signup')
-      .send(testData)
+      .send(userTwo)
       .end((err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body.status).to.equal('success');
@@ -36,16 +36,15 @@ export const userSpec = (repiceSpec) => {
     });
     it('return 200 as status code', done => {
       request.post('/api/v1/users/signup')
-      .send(regData)
+      .send(userOne)
       .end((err, res) => {
         expect(res.status).to.equal(200);
         expect(res.body.status).to.equal('success');
         done();
       });
     });
-
     it('return 400 for an already existing email ', done => {
-      const invalidData = testData;
+      const invalidData = userTwo;
       invalidData.username = 'exampleuser2';
       request.post('/api/v1/users/signup')
       .send(invalidData)
@@ -55,9 +54,8 @@ export const userSpec = (repiceSpec) => {
         done();
       });
     });
-
     it('return 400 for an already existing username', done => {
-      const invalidData = testData;
+      const invalidData = userTwo;
       invalidData.email = 'example2@user.com';
       request.post('/api/v1/users/signup')
       .send(invalidData)
@@ -67,9 +65,8 @@ export const userSpec = (repiceSpec) => {
         done();
       });
     });
-
     it('return 400 for invalid email', done => {
-      const invalidData = testData;
+      const invalidData = userTwo;
       invalidData.email = 'example2user.com';
       request.post('/api/v1/users/signup')
       .send(invalidData)
@@ -80,7 +77,7 @@ export const userSpec = (repiceSpec) => {
       });
     });
     it('return 400 for invalid username', done => {
-      const invalidData = testData;
+      const invalidData = userTwo;
       invalidData.username = 'e.com';
       request.post('/api/v1/users/signup')
       .send(invalidData)
@@ -91,7 +88,7 @@ export const userSpec = (repiceSpec) => {
       });
     });
     it('return 400 for invalid password', done => {
-      const invalidData = testData;
+      const invalidData = userTwo;
       invalidData.password = 'ecom';
       request.post('/api/v1/users/signup')
       .send(invalidData)
@@ -105,25 +102,23 @@ export const userSpec = (repiceSpec) => {
 
   // user login
   describe('User Login', () => {
-    let testDataLogin = {};
-    let loginData = {};
+    let userOne = {};
+    let userTwo = {};
 
     beforeEach(() => {
-      testDataLogin = {
+      userOne = {
         username: 'example_user',
         password: '123456',
       };
-      loginData = {
+      userTwo = {
         username: 'example_user_2',
         password: '123456',
       };
     });
 
-    after(() => repiceSpec(token, token2));
-
     it('return 200 as status code', done => {
       request.post('/api/v1/users/signin')
-      .send(testDataLogin)
+      .send(userOne)
       .end((err, res) => {
         token = res.body.User.token;
         expect(res.status).to.equal(200);
@@ -135,7 +130,7 @@ export const userSpec = (repiceSpec) => {
     });
     it('return 200 as status code', done => {
       request.post('/api/v1/users/signin')
-      .send(loginData)
+      .send(userTwo)
       .end((err, res) => {
         token2 = res.body.User.token;
         expect(res.status).to.equal(200);
@@ -163,7 +158,7 @@ export const userSpec = (repiceSpec) => {
       });
     });
     it('return 400 for invalid username', done => {
-      const invalidData = testDataLogin;
+      const invalidData = userOne;
       invalidData.username = 'kester';
       request.post('/api/v1/users/signin')
       .send(invalidData)
@@ -174,7 +169,7 @@ export const userSpec = (repiceSpec) => {
       });
     });
     it('return 400 for invalid password', done => {
-      const invalidData = testDataLogin;
+      const invalidData = userOne;
       invalidData.password = 'ecom';
       request.post('/api/v1/users/signin')
       .send(invalidData)
@@ -185,7 +180,7 @@ export const userSpec = (repiceSpec) => {
       });
     });
     it('return 400 for invalid password', done => {
-      const invalidData = testDataLogin;
+      const invalidData = userOne;
       invalidData.password = '';
       request.post('/api/v1/users/signin')
       .send(invalidData)
@@ -195,6 +190,107 @@ export const userSpec = (repiceSpec) => {
         done();
       });
     });
+  });
+
+   // edit user profile
+  describe('Profile update', () => {
+    const updateOne = {
+      fullname: 'example user two',
+      username: 'example_user_2',
+      password: '123456',
+      email: 'exampleTwo@user.com',
+    };
+    const updateTwo = {
+      fullname: 'example user two',
+    };
+    after(() => repiceSpec(token, token2));
+
+    it('return 200 as status code when everything is fine', done => {
+      request.put('/api/v1/users/user')
+      .set('Authorization', token2)
+      .send(updateOne)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.User.fullname).to.equal('example user two');
+        done();
+      });
+    });
+    it('return 200 as status code when updating a single field', done => {
+      request.put('/api/v1/users/user')
+      .set('Authorization', token2)
+      .send(updateTwo)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.User.fullname).to.equal('example user two');
+        done();
+      });
+    });
+    it('return 403 as status code for update user profile request with no token', done => {
+      request.put('/api/v1/users/user')
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        expect(res.body.status).to.equal('fail');
+        done();
+      });
+    });
+    // it('return 400 for an already existing email ', done => {
+    //   const invalidData = updateOne;
+    //   invalidData.username = 'exampleuser2';
+    //   request.put('/api/v1/users/user')
+    //   .set('Authorization', token)
+    //   .send(invalidData)
+    //   .end((err, res) => {
+    //     expect(res.status).to.equal(400);
+    //     expect(res.body.status).to.equal('fail');
+    //     done();
+    //   });
+    // });
+    // it('return 400 for an already existing username', done => {
+    //   const invalidData = updateOne;
+    //   invalidData.email = 'example2@user.com';
+    //   request.post('/api/v1/users/user')
+    //   .send(invalidData)
+    //   .end((err, res) => {
+    //     expect(res.status).to.equal(400);
+    //     expect(res.body.status).to.equal('fail');
+    //     done();
+    //   });
+    // });
+    // it('return 400 for invalid email', done => {
+    //   const invalidData = userTwo;
+    //   invalidData.email = 'example2user.com';
+    //   request.post('/api/v1/users/signup')
+    //   .send(invalidData)
+    //   .end((err, res) => {
+    //     expect(res.status).to.equal(400);
+    //     expect(res.body.status).to.equal('fail');
+    //     done();
+    //   });
+    // });
+    // it('return 400 for invalid username', done => {
+    //   const invalidData = userTwo;
+    //   invalidData.username = 'e.com';
+    //   request.post('/api/v1/users/signup')
+    //   .send(invalidData)
+    //   .end((err, res) => {
+    //     expect(res.status).to.equal(400);
+    //     expect(res.body.status).to.equal('fail');
+    //     done();
+    //   });
+    // });
+    // it('return 400 for invalid password', done => {
+    //   const invalidData = userTwo;
+    //   invalidData.password = 'ecom';
+    //   request.post('/api/v1/users/signup')
+    //   .send(invalidData)
+    //   .end((err, res) => {
+    //     expect(res.status).to.equal(400);
+    //     expect(res.body.status).to.equal('fail');
+    //     done();
+    //   });
+    // });
   });
 };
 
