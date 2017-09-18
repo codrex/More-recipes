b1d0e99f1a3379c077612d06685a32f9a64b3058
resy <ogbemudia@gmail.com>
2017-09-18 17:12:57 +0100

    setup client side dev env

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './vendor/css/bootstrap.min.css';
+import App from './components/app';
+
+render(<App />, document.getElementById('root'));
